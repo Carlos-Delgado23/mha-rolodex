630b2d6eb4db70b6e26c7d49b05bd25af52c78c3
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-11-23 17:51:06 -0600

    searchfiled filter characters

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Card = (props) => {
   return (
-    <div className="flex flex-col items-center bg-yellow-400 border rounded-md border-yellow-500 p-4 cursor-pointer transition duration-200 ease-out transform hover:-translate hover:scale-110 w-56">
+    <div className="flex flex-col items-center bg-yellow-400 border rounded-md border-yellow-500 p-4 cursor-pointer transition duration-200 ease-out transform hover:-translate hover:scale-110">
       <img alt="character" src={props.character.images[0]} className="h-40 w-40" />
       <h2 className="text-lg uppercase font-bold font-mono mt-4 mb-3 underline">
         {
