ba13ea714b1bc8fc8e307dafd35f11c681008546
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-11-27 21:20:09 -0600

    style input

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,7 +4,7 @@ export const Card = (props) => {
   return (
     <div className="flex flex-col items-center bg-yellow-400 border rounded-md border-yellow-500 p-4 cursor-pointer transition duration-200 ease-out transform hover:-translate hover:scale-110">
       <img alt="character" src={props.character.images[0]} className="h-40 w-40" />
-      <h2 className="text-lg uppercase font-bold font-mono mt-4 mb-3 underline">
+      <h2 className="text-lg uppercase font-bold font-mono mt-4 mb-3">
         {
           props.character.name == null
             ? props.character.id.replace(/_/g, ' ')
@@ -12,13 +12,19 @@ export const Card = (props) => {
         }
       </h2>
       <div className="text-base font-medium">
-        <p>
-          <span className="">Quirk: </span>
+        <p className="mb-2">
+          <span className="italic">Quirk: </span>
           {props.character.quirk == null
             ? 'Unlisted'
             : props.character.quirk}
         </p>
-        <p className="mt-2">{props.character.occupation}</p>
+        <p>
+          <span className="italic">Alias: </span>
+          {props.character.alias == null
+            ? 'Unlisted'
+            : props.character.alias}
+        </p>
+        <p className="mt-2"><span className="italic">Occ: </span> {props.character.occupation}</p>
       </div>
     </div >
   )
