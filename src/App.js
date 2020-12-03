4a39cbdb45b64e89c3642591e15f652f2c99f3ce
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-12-03 12:40:41 -0600

    moved search input a comp.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component'
 
 import './App.css';
 
@@ -27,13 +28,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <input
-        type="text"
-        value={searchField}
-        placeholder="Search Characters"
-        onChange={handleChange}
-        className="my-6 py-2 px-3 border border-yellow-800 rounded-xl shadow-lg shadow-outline outline-none"
-      />
+      <SearchBox value={searchField} placeholder="Search Characters" handleChange={handleChange} />
       <CardList characters={characters} />
     </div>
   );
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const SearchBox = ({ placeholder, handleChange, searchField }) => {
+  return (
+    <div>
+      <input
+        type="text"
+        value={searchField}
+        placeholder={placeholder}
+        onChange={handleChange}
+        className="my-6 py-2 px-3 border border-yellow-800 rounded-xl shadow-lg shadow-outline outline-none"
+      />
+    </div>
+  )
+}
