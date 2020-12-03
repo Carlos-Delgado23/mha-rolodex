080a47fbbf9d881650842d9a9d3527e4217f24cd
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-12-03 16:08:56 -0600

    pagination buttons with api call

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 import { CardList } from './components/card-list/card-list.component'
 import { SearchBox } from './components/search-box/search-box.component'
+import { ButtonPage } from './components/button-page/button-page.component'
 
 import './App.css';
 import mhaLogo from './images/mhaLogo.png'
@@ -11,28 +12,54 @@ const App = () => {
   const apiUrl = 'https://myheroacademiaapi.com/api/character'
   const [characters, setCharacters] = useState([])
   const [searchField, setSearchField] = useState('')
+  const [pageNum, setPageNum] = useState(1)
 
   const handleChange = e => {
     setSearchField(e.target.value)
   }
 
+  const nextPage = (e) => {
+    return pageNum < 16
+      ? setPageNum(pageNum + 1)
+      : pageNum
+  }
+
+  const prevPage = (e) => {
+    return pageNum > 1
+      ? setPageNum(pageNum - 1)
+      : pageNum
+  }
+
   useEffect(() => {
     async function fetchCharacters() {
-      const resp = await fetch(searchField < 0 ? apiUrl : apiUrl + `?alias=${searchField}`)
+      const resp = await fetch(
+        searchField < 0
+          ? apiUrl
+          : apiUrl + `?alias=${searchField}&page=${pageNum}`)
       const data = await resp.json()
 
       setCharacters(data.result)
     }
     fetchCharacters()
-  }, [apiUrl, searchField])
+  }, [apiUrl, searchField, pageNum])
 
 
   return (
     <div className="App">
       <img src={mhaLogo} alt="mha-logo" className="cover-fill mx-auto mt-12" />
 
-      <h1 className="text-2xl font-bold uppercase font-mono mt-12">Rolodex Search</h1>
-      <SearchBox value={searchField} placeholder="Character Alias" handleChange={handleChange} />
+      <div className="mb-6">
+        <h1 className="text-2xl font-bold uppercase font-mono mt-12">Rolodex Search</h1>
+        <SearchBox value={searchField} placeholder="Character Alias" handleChange={handleChange} />
+
+        <div className="inline-flex">
+          <ButtonPage handleClick={prevPage} title='Previous Page' pageNum={pageNum} />
+
+          <ButtonPage handleClick={nextPage} title='Next Page' />
+        </div>
+      </div>
+
+
       <CardList characters={characters} />
     </div>
   );
